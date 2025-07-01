fe57e95485d08ec9d732441a7839c7f24755ddff
Rajesh M Venkatesan <mvrajeshiyer@gmail.com>
2025-07-02 00:28:33 +0530

    Update secrets_manager.py

--- utils/secrets_manager.py
+++ utils/secrets_manager.py
@@ -1,16 +1,12 @@
-import os
 import json
 from cryptography.fernet import Fernet
 
-export SECRET_KEY="O8lc6uPUdNdksa9DnnfaF6Xv-McvCsGwq_RbrpfDF2w="
-
-def load_secrets(enc_file='encrypted_keys.enc', key_env_var='SECRET_KEY'):
+def load_secrets(enc_file='encrypted_keys.enc', key_file='fernet.key'):
     """
-    Decrypts and loads API keys from an encrypted file using a Fernet key from an environment variable.
+    Decrypts and loads API keys from an encrypted file using a Fernet key from a file.
     """
-    secret_key = os.environ.get(key_env_var)
-    if not secret_key:
-        raise ValueError("SECRET_KEY environment variable not set.")
+    with open(key_file, "rb") as kf:
+        secret_key = kf.read()
     fernet = Fernet(secret_key)
     with open(enc_file, 'rb') as f:
         encrypted = f.read()
